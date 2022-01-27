88e924245eb33965fd4a78efb80268f85da2e5bb
Bazyliii <67436562+Bazyliii@users.noreply.github.com>
2022-01-27 21:32:05 +0100

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,8 @@
+
+const loading = document.querySelector('.loading-dots');
+const tab2 = document.querySelector('#tab2');
+const tab2i = document.querySelector('.calendar');
+
 //Dropdown menu
 document.addEventListener("click", e => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]")
@@ -12,8 +17,8 @@ document.addEventListener("click", e => {
     dropdown.classList.remove("active")
   })
 });
-//Fetching and display of plan
-fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia.p.lodz.pl/dla-studentow/plany-zajec/studia-stacjonarne-plany-zajec/')}`)
+//Fetching i wyświetlanie planu
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia.p.lodz.pl/dla-studentow/plany-zajec-i-egzaminow/studia-stacjonarne-plany-zajec/')}`)
   .then(response => {
     if (response.ok) return response.json()
     throw new Error('Network response was not ok.')
@@ -27,7 +32,7 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia
         for (k = 0; k < 3; k++) {
           linkContent = data.contents.match("wgrane_pliki/" + years[k] + months[i] + j + ".pdf");
           if (linkContent != null) {
-            link = "http://www.weeia.p.lodz.pl/" + linkContent + "#page=5";
+             link = "http://www.weeia.p.lodz.pl/" + linkContent;
           }
         }
       }
@@ -44,16 +49,20 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia
       if (pageNumber >= 1 && pageNumber <= doc._pdfInfo.numPages) {
         const page = await doc.getPage(pageNumber);
         const viewport = page.getViewport({
-          scale: 4
+          scale: 3
         });
         const canvas = document.getElementById("canvas");
         const context = canvas.getContext("2d");
-        canvas.height = 576 * 4;
-        canvas.width = 1263 * 2.7;
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        loading.style.display = "none";
+        tab2.style.pointerEvents = "auto";
+        tab2i.style.opacity = "1";
         return await page.render({
           canvasContext: context,
           viewport: viewport
         }).promise;
+       
       } else {
         console.log("Error!");
       }
@@ -85,23 +94,36 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://weeia.ed
     } catch (e) {}
     console.log(filteredItems)
   });
+//Fetching wydziałów
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://weeia.edu.p.lodz.pl')}`)
+  .then(response => {
+    if (response.ok) return response.json()
+    throw new Error('Network response was not ok.')
+  })
+  .then(data => {
+    console.log(data.contents)
+  });
+
+
+
+
+
 
 function showhide() {
-  var x = document.getElementById("planlekcji");
-  var tab = ["tab1", "tab2", "tab3", "tab4", "tab5", "tab6", "tab7", "tab8"];
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    for (i = 0; i <= 7; i++) {
-      y = document.getElementById(tab[i]);
-      y.style.display = "block";
-      y.style.pointerEvents = "auto";
-    }
+  let plan = document.getElementById("planlekcji");
+  let wrapper = document.querySelector(".wrapper");
+  if (plan.style.display === "flex") {
+    plan.style.display = "none";
+      wrapper.style.display = "grid";
+      wrapper.style.pointerEvents = "auto";
   } else {
-    x.style.display = "block";
-    for (i = 0; i <= 7; i++) {
-      y = document.getElementById(tab[i]);
-      y.style.display = "none";
-      y.style.pointerEvents = "none";
+    plan.style.display = "flex";
+      wrapper.style.display = "none";
+      wrapper.style.pointerEvents = "none";
     }
   }
-}
+
+
+
+
+
