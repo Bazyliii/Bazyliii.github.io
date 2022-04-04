f35bebd7c8ad5b0d2ec5653a96360c0517be705d
Bazyliii <67436562+Bazyliii@users.noreply.github.com>
2022-04-04 22:22:15 +0200

    Add files via upload

--- Javascript/plan.js
+++ Javascript/plan.js
@@ -1,34 +1,29 @@
 fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia.p.lodz.pl/dla-studentow/plany-semestru-letniego/studia-stacjonarne/')}`)
 	.then(response => {
 		if (response.ok) return response.json()
-		throw new Error('Error!')
-	})
+		throw new Error('Error!')})
 		.then(data => {
-			currentYear = parseInt(new Date().getFullYear().toString().substr(-2));
-			years = [currentYear - 1, currentYear, currentYear + 1];
+			currentYear = parseInt(new Date().getFullYear().toString().substr(-2))
+			years = [currentYear - 1, currentYear, currentYear + 1]
 			for(i=0; i<3; i++){
-				linkPart = data.contents.split("href='wgrane_pliki/" + years[i]).pop();
-				linkPart2 = "http://www.weeia.p.lodz.pl/wgrane_pliki/" + years[i] + linkPart.substring(0, linkPart.indexOf("'")).split("\n").filter(item => item !== '');
+				linkPart = data.contents.split("href='wgrane_pliki/" + years[i]).pop()
+				linkPart2 = "http://www.weeia.p.lodz.pl/wgrane_pliki/" + years[i] + linkPart.substring(0, linkPart.indexOf("'")).split("\n").filter(item => item !== '')
 			if(linkPart2.length<100){
-				localStorage.setItem("link",linkPart2)
-			}
-		} 	
-	})
-	link = localStorage.getItem("link")
-	const proxy = "https://wikamproxy.herokuapp.com/";
+			var link = linkPart2
+			}} 	
+	const proxy = "https://wikamproxy.herokuapp.com/"
 		const url = proxy + link;
 		(async function () {
-			pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
-			const doc = await pdfjsLib.getDocument(url).promise;
-			let currentPage = 15;
-			await getPage(doc, currentPage);
-		})();
+			pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js"
+			const doc = await pdfjsLib.getDocument(url).promise
+			let currentPage = 15
+			await getPage(doc, currentPage)})()
 		async function getPage(doc, pageNumber) {
 			if (pageNumber >= 1 && pageNumber <= doc._pdfInfo.numPages) {
-				const page = await doc.getPage(pageNumber);
+				const page = await doc.getPage(pageNumber)
 				const viewport = page.getViewport({
 					scale: 3
-				});
+				})
 				const canvas = document.querySelector("canvas")
 				const context = canvas.getContext("2d")
 				canvas.height = viewport.height
@@ -36,16 +31,14 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('http://www.weeia
 				return await page.render({
 					canvasContext: context,
 					viewport: viewport
-				}).promise;
+				}).promise
 			} else {
-				console.log("Error!");
-			}
-		}
-    const plan = document.getElementById("plan-container")
-    const tasks = document.getElementById("task-grid")
-	const subjects = document.getElementById("subjects-grid")
-	const faculty = document.getElementById("faculty-grid")
-
+				console.log("Error!")
+			}}})
+const plan = document.getElementById("plan-container")
+const tasks = document.getElementById("task-grid")
+const subjects = document.getElementById("subjects-grid")
+const faculty = document.getElementById("faculty-grid")
 function showhide(x, y){
     if(x.style.opacity == '0'){
         x.style.opacity = '1'
@@ -61,6 +54,5 @@ function showhide(x, y){
 		setTimeout(function () {
 			x.style.display = 'none'
 			tasks.style.display = 'grid'
-		}, 150)
-    }
-}
+		}, 150)}}
+
