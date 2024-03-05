5ad3bdc23945b7784c901f126375bda1bc7423fd
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-03-06 00:15:27 +0100

    Begining of EBW page. For now it is just a skeleton but JS sorta works and timetable is generated :D

---
+++ plan.js
@@ -0,0 +1,15 @@
+const day = ["pon", "wt", "sr", "czw", "pt"]
+
+fetch("./plan6AiSR4.json")
+	.then((response) => response.json())
+	.then((data) => {
+		data.forEach((element) => {
+			const mainElem = document.getElementById(day[element.Day])
+			const newDiv = document.createElement("div")
+			newDiv.appendChild(document.createTextNode(element.SubjectName))
+			newDiv.style.gridColumnStart = element.Start + 1
+			newDiv.style.gridColumnEnd = element.End + 1
+			newDiv.className = "plan-elem"
+			mainElem.appendChild(newDiv)
+		})
+	})
