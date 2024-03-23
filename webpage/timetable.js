5b34130b37072ded42a4455fb5cbf1ad7966dafd
Jarosław Wierzbowski <jaroslawierzbowski2001@gmail.com>
2024-03-23 01:02:25 +0100

    Swipe left right

--- webpage/timetable.js
+++ webpage/timetable.js
@@ -14,6 +14,9 @@ const processData = (weekData) => {
 		contentWrapper.append(newDiv)
 	})
 }
+let touchstartX = 0
+let touchendX = 0
+const minSwipeDistance = 100
 
 fetch("https://raw.githubusercontent.com/Bazyliii/Bazyliii.github.io/main/plan6AiSR4.json")
 	.then((response) => response.json())
@@ -22,15 +25,36 @@ fetch("https://raw.githubusercontent.com/Bazyliii/Bazyliii.github.io/main/plan6A
 		const weekData = data.filter((element) => element.Day === weekNumber - lol)
 		processData(weekData)
 
-		previousButton.addEventListener("click", () => {
-			lol += 1
-			const weekData = data.filter((element) => element.Day === weekNumber - lol)
-			processData(weekData)
+		// previousButton.addEventListener("click", () => {
+		// 	lol += 1
+		// 	const weekData = data.filter((element) => element.Day === weekNumber - lol)
+		// 	processData(weekData)
+		// })
+
+		// nextButton.addEventListener("click", () => {
+		// 	lol -= 1
+		// 	const weekData = data.filter((element) => element.Day === weekNumber - lol)
+		// 	processData(weekData)
+		// })
+
+		document.addEventListener("touchstart", (e) => {
+			touchstartX = e.changedTouches[0].screenX
 		})
 
-		nextButton.addEventListener("click", () => {
-			lol -= 1
-			const weekData = data.filter((element) => element.Day === weekNumber - lol)
-			processData(weekData)
+		document.addEventListener("touchend", (e) => {
+			touchendX = e.changedTouches[0].screenX
+			const distance = Math.abs(touchendX - touchstartX)
+			if (distance >= minSwipeDistance) {
+				if (touchendX < touchstartX) {
+					lol -= 1
+					const weekData = data.filter((element) => element.Day === weekNumber - lol)
+					processData(weekData)
+				}
+				if (touchendX > touchstartX) {
+					lol += 1
+					const weekData = data.filter((element) => element.Day === weekNumber - lol)
+					processData(weekData)
+				}
+			}
 		})
 	})
